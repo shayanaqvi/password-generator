568b59cd90a90c45322e515443cc843d963a0c90
shayanaqvi <shayan.naqvi@icloud.com>
2022-07-21 15:45:50 +0500

    Checking if the password length is an integer or not

--- generator.py
+++ generator.py
@@ -1,9 +1,24 @@
 import time
 import random
+from os import *
 
-def generate_password():
-	
+def get_password_length():
+	time.sleep(0.5)
 	password_length = input("\nHow many characters should your new password be? ")
+
+	if password_length.isdigit() == False:
+		print("\nHey! That's not a number!")
+		system('exit')
+	elif int(password_length) <= 7:
+		print("Hmmmmmmmmmmm...")
+		time.sleep(0.75)
+		print("That's not long enough. A good password is at least 8 characters.")
+		time.sleep(0.75)
+		system('exit')
+	elif password_length.isdigit() == True:
+		generate_password(password_length)
+
+def generate_password(password_length):
 
 	letters = ["a", "b", "c", "d", "e", "f", 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 	numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
@@ -29,6 +44,8 @@ def generate_password():
 		print(password, end='')
 
 	print("\n\nKeep it safe!")
+	time.sleep(0.5)
 
-generate_password()
-print("\n")
+get_password_length()
+
+print("")
