497392d15eec90e458b32f7e528baa08bdc67bd8
shayanaqvi <shayan.naqvi@icloud.com>
2022-07-21 15:07:09 +0500

    Added special characters to password generation

--- generator.py
+++ generator.py
@@ -7,6 +7,7 @@ def generate_password():
 
 	letters = ["a", "b", "c", "d", "e", "f", 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 	numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+	special_chars = ["~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "=", "+", "[", "]", ";", ":", "<", "|", ">", "?", "/"]
 
 	password = []
 
@@ -14,13 +15,14 @@ def generate_password():
 
 	while increment <= int(password_length):
 		increment += 1
-		select_character = random.randint(0, 1)
+		select_character = random.randint(0, 2)
 
 		if select_character == 0:
-			password =  letters[random.randint(0, 25)]
+			password = letters[random.randint(0, 25)]
 		elif select_character == 1:
 			password = numbers[random.randint(0, 9)]
-
+		elif select_character == 2:
+			password = special_chars[random.randint(0, 23)]
 
 		print(password, end='')
 
