10b8d1b96d7b7510f614e934b1e29c941a53acb4
shayanaqvi <shayan.naqvi@icloud.com>
2022-07-21 14:12:12 +0500

    Added randomized number generation

--- generator.py
+++ generator.py
@@ -5,12 +5,12 @@ def generate_password():
 	
 	password_length = input("How long should your new password be? ")
 
-	letters = ["a", "b", "c", "d", "e", "f"]
-	numbers = [1, 2, 3, 4, 5, 6]
+	letters = ["a", "b", "c", "d", "e", "f", 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+	numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
 
 	for i in range(0, int(password_length)):
-		print(letters[random.randint(0, 5)], end='')
-
+		print(letters[random.randint(0, 25)], end='')
+		print(numbers[random.randint(0, 9)], end='')
 	print("")
 
 generate_password()
