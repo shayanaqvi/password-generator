dfac92708ea598fb19c6fe58b38bbd698e9daefe
shayanaqvi <shayan.naqvi@icloud.com>
2022-07-31 21:15:59 +0500

    Minor tweaks

--- generator.py
+++ generator.py
@@ -5,17 +5,24 @@ from PyQt5.QtWidgets import *
 from PyQt5 import uic
 from os import *
 
-#TODO get the password to generate correctly with all three options
-
 
 class gui(QMainWindow):
 	def __init__(self):
+
 		super(gui, self).__init__()
+
+		self.setFixedSize(800, 420)
+
 		uic.loadUi("interface.ui", self)
 		self.show()
 
 		self.actionClose.triggered.connect(exit)
 		self.generate_pwsd_button.clicked.connect(self.generate_password)
+		self.generate_uname_button.clicked.connect(self.generate_uname)
+
+	def generate_uname(self):
+		pref_words = []
+		suf_words = []
 
 	def generate_password(self):
 		pswd_length = int(self.pswd_length_field.text())
@@ -101,26 +108,3 @@ def main():
 if __name__ == '__main__':
 	main()
 
-
-#def generate_password(password_length):
-#	password_length_int = int(password_length)
-#
-#	letters = ["a", "b", "c", "d", "e", "f", 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-#	numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
-#	special_chars = ["~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "=", "+", "[", "]", ";", ":", "<", "|", ">", "?", "/"]
-#
-#
-#	password = []
-#
-#	for number in range(password_length_int):
-#		random_letter = letters[random.randint(0, 25)]
-#		random_number = numbers[random.randint(0, 9)]
-#		random_special_character = special_chars[random.randint(0, 23)]
-##		key = random.randint(0, 2)
-
-#		if key == 0:
-#			password.append(random_letter)
-#		elif key == 1:
-#			password.append(random_number)
-#		elif key == 2:
-#			password.append(random_special_character)
