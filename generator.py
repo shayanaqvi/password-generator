e7f4589c7829dfe4c332282c8706486e027f6ead
Shayan Naqvi <93083490+shayanaqvi@users.noreply.github.com>
2022-07-31 11:04:58 +0500

    Add files via upload

--- generator.py
+++ generator.py
@@ -5,7 +5,7 @@ from PyQt5.QtWidgets import *
 from PyQt5 import uic
 from os import *
 
-
+#TODO get the password to generate correctly with all three options
 
 
 class gui(QMainWindow):
@@ -21,30 +21,76 @@ class gui(QMainWindow):
 		pswd_length = int(self.pswd_length_field.text())
 
 		letters = ["a", "b", "c", "d", "e", "f", 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-		numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
-		special_chars = ["~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "=", "+", "[", "]", ";", ":", "<", "|", ">", "?", "/"]
+		numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+		spec_chars = ["~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "=", "+", "[", "]", ";", ":", "<", "|", ">", "?", "/"]
 
 		password = []
 
 		for number in range(pswd_length):
-			random_letter = letters[random.randint(0, 50)]
-			random_number = numbers[random.randint(0, 9)]
-			random_special_character = special_chars[random.randint(0, 23)]
+
 			key = random.randint(0, 2)
 
 			if key == 0:
-				password.append(random_letter)
+				if self.incl_spec_chars_cbox.isChecked() == True:
+					password.append(spec_chars[random.randint(0, 23)])
+				else:
+					subkey = random.randint(0, 1)
+
+					if self.incl_nums_cbox.isChecked() == True:
+						if subkey == 0:
+							password.append(numbers[random.randint(0, 9)])
+						if subkey == 1:
+							password.append(numbers[random.randint(0, 9)])
+					if self.incl_lets_cbox.isChecked() == True:
+						if subkey == 0:
+							password.append(letters[random.randint(0, 50)])
+						if subkey == 1:
+							password.append(letters[random.randint(0, 50)])
 			elif key == 1:
-				password.append(random_number)
+				if self.incl_nums_cbox.isChecked() == True:
+					password.append(numbers[random.randint(0, 9)])
+				else:
+					subkey = random.randint(0, 1)
+
+					if self.incl_spec_chars_cbox.isChecked() == True:
+						if subkey == 0:
+							password.append(spec_chars[random.randint(0, 23)])
+						if subkey == 1:
+							password.append(spec_chars[random.randint(0, 23)])
+
+					if self.incl_lets_cbox.isChecked() == True:
+						if subkey == 0:
+							password.append(letters[random.randint(0, 50)])
+						if subkey == 1:
+							password.append(letters[random.randint(0, 50)])
+
+
 			elif key == 2:
-				password.append(random_special_character)
+				if self.incl_lets_cbox.isChecked() == True:
+					password.append(letters[random.randint(0, 50)])
+				else:
+					subkey = random.randint(0, 1)
+
+					if self.incl_nums_cbox.isChecked() == True:
+						if subkey == 0:
+							password.append(numbers[random.randint(0, 9)])
+						if subkey == 1:
+							password.append(numbers[random.randint(0, 9)])
+					if self.incl_spec_chars_cbox.isChecked() == True:
+						if subkey == 0:
+							password.append(spec_chars[random.randint(0, 23)])
+						if subkey == 1:
+							password.append(spec_chars[random.randint(0, 23)])
+
+
 
 		password_string = ""
 
 		for character in password:
 			password_string += str(character)
 
-		self.new_pswd_field.setText(password_string)
+		self.new_pswd_field.setText(password_string[:pswd_length])
+
 
 
 def main():
